b49b07f5a87cd0eb8d75487dbd1461eef4499e21
hdrdnz <h_durudeniz@hotmail.com>
2025-02-01 17:33:36 +0300

    test2

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-echo "🚀 Deploy başlatılıyor..."
 
-# Docker Hub’dan en güncel image’ı çek
-docker pull elifgider/karbonizgi:latest
-
-# Çalışan eski container’ı durdur ve kaldır
-docker stop karbonizgi-container
-docker rm karbonizgi-container
-
-# Yeni container’ı başlat
-docker run -d --name karbonizgi-container -p 8080:8080 elifgider/karbonizgi:latest
-
-echo "✅ Deploy tamamlandı!"
